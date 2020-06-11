54d518a6686b786efe18bd3f10b3c9a5b6b683db
nathanAquino <nathado2017@gmail.com>
2020-06-11 19:49:10 -0300

    aprendendo C#

---
+++ Aula 1 - C#/AprendendoCSharp/1-Primeiro Projeto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar ...");
+            Console.ReadLine();
+        }
+    }
+}
